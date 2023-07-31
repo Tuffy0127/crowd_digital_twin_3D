77b731d8f447da7272fdedda66d7c4392ee6c1a1
Tuffy0127 <tuffy12133@gmail.com>
2023-07-31 16:56:12 +0800

    3d

---
+++ srp_3d.cpp
@@ -0,0 +1,539 @@
+#include"srp_3d.h"
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<sstream>
+#include<string>
+#include<stdio.h>
+#include<omp.h>
+#include<time.h>
+
+using namespace std;
+
+const int thread_num = 12; // OpenMP线程数
+
+// 可调参数
+int agent_num = 500;
+int step_num = 5000;
+double tick = 0.05;//timestep
+int jam_time_threshole = 50;
+double total_time = 0;
+
+// 程序数据
+int  obstical_line_num[total_level] = {};
+clock_t total_start, total_end;
+vector<AGENT> agent_list; // agent vector
+//struct OBLINE obstical_lines[total_level][MAX_OBLINE_NUM]; // obstical line array
+vector<vector<OBLINE>> obline_list;
+vector<vector<double>> seq;
+vector<AGENT> agent_back_list; // agent vector
+
+// 输出文件
+FILE* f = fopen("C:/Users/leesh/Desktop/srp/output_3d/output.txt", "w");
+FILE* ff = fopen("C:/Users/leesh/Desktop/srp/output_3d/test.txt", "w");
+
+// 终点
+struct cordinate goal[1] = { {80,57.5,1} };
+
+
+
+
+
+// 函数声明
+void init_obline(string[],int);//障碍物读入
+void init_map(string[], int);
+void init_agent_seq(string);
+void init_agent(int);
+void init();
+void output(AGENT*);//写出agent坐标
+void step();
+void update_density();
+void test();
+void push_new_agent();
+
+
+// 函数实现
+void init_obline(string obline_file[], int level)
+{
+	for (int k = 0; k < level; ++k)
+	{
+		fstream infile(obline_file[k], ios::in);
+		string buf;
+		int num = 0;
+		getline(infile, buf);
+		vector<OBLINE> temp1;
+		while (!infile.eof()) 
+		{
+			OBLINE temp;
+			stringstream ss(buf);
+			ss >> temp.sy >> temp.sx >> temp.ey >> temp.ex;
+			temp.sx /= map_factor;
+			temp.sy /= map_factor;
+			temp.ex /= map_factor;
+			temp.ey /= map_factor;
+			temp.len = sqrt((temp.sx - temp.ex) * (temp.sx - temp.ex) + (temp.sy - temp.ey) * (temp.sy - temp.ey));
+			getline(infile, buf);
+			temp1.push_back(temp);
+			num++;
+		}
+		obline_list.push_back(temp1);
+		obstical_line_num[k] = num;
+		infile.close();
+	}
+	
+}
+
+
+void init_map(string map_file[], int level)
+{
+	for (int k = 0; k < level; ++k)
+	{
+		vector<vector<int>> temp1;
+		fstream infile(map_file[k], ios::in);
+		string buf = "";
+		getline(infile, buf);
+		while (!infile.eof())
+		{
+			vector<int> temp2;
+			++row_num[k];
+			stringstream ss(buf);
+			int num;
+			while (!ss.eof())
+			{
+				if (row_num[k]== 1)
+				{
+					++col_num[k];
+				}
+				ss >> num;
+				temp2.push_back(num);
+			}
+			getline(infile, buf);
+			temp1.push_back(temp2);
+		}
+		++col_num[k];
+		map_matrix.push_back(temp1);
+		infile.close();
+
+		vector<vector<node>> temp3;
+		for (int i = 0; i < row_num[k]; i++)
+		{
+			vector<node> temp4;
+			for (int j = 0; j < col_num[k]; j++)
+			{
+				temp4.push_back(node(j, i));
+			}
+			temp3.push_back(temp4);
+		}
+		map_matrix_A.push_back(temp3);
+
+
+		vector<vector<int>> temp5;
+		for (int i = 0; i < row_num[k]; i++)
+		{
+			vector<int> temp6;
+			for (int j = 0; j < col_num[k]; j++)
+			{
+				temp6.push_back(0);
+			}
+			temp5.push_back(temp6);
+		}
+		density_map.push_back(temp5);
+
+	}
+	
+
+}
+
+
+void init_agent_seq(string seq_file)
+{
+	agent_num = 0;
+	fstream infile(seq_file, ios::in);
+	string buf = "";
+	getline(infile, buf);
+	while (!infile.eof())
+	{
+		agent_num++;
+		stringstream ss(buf);
+		seq.push_back(vector<double>());
+		double temp;
+		while (!ss.eof())
+		{
+			ss >> temp;
+			seq[seq.size() - 1].push_back(temp);
+		}
+		getline(infile, buf);
+	}
+	cout << "Initializing " << agent_num << " agent(s)..." << endl;;
+	for (int i = 0; i < agent_num; i++)
+	{
+		AGENT a;
+		a.id = int(seq[i][0]);
+		a.x = seq[i][1];
+		a.y = seq[i][2];
+		a.level = int(seq[i][3]);
+		a.m = 80;
+		a.gx = seq[i][4];
+		a.gy = seq[i][5];
+		a.fgx = seq[i][4];
+		a.fgy = seq[i][5];
+		a.goal_level = int(seq[i][6]);
+		a.vx = randval(-2, 2);
+		a.vy = randval(-2, 2);
+		a.v0 = MAX_V;
+		a.next_gx = seq[i][4];
+		a.next_gy = seq[i][5];
+		a.dis = sqrt((a.x - a.gx) * (a.x - a.gx) + (a.y - a.gy) * (a.y - a.gy)) * (abs(a.goal_level - a.level) + 1);
+		int rand = int(randval(0, 6));
+		a.color = rand;
+		a.arrive_time = seq[i][7];
+		agent_back_list.push_back(a);
+	}
+
+
+}
+
+
+void init_agent(int agent_num)
+{
+	cout << "Initializing " << agent_num << " agent(s)" << endl;;
+	for (int i = 0; i < agent_num; i++)
+	{
+		int rand = int(randval(0, 1));
+		//cout << rand << endl;
+		AGENT a;
+		a.id = i;
+		//a.level = int(randval(0, total_level));
+		a.level = 0;
+		do
+		{
+			a.x = randval(0, col_num[a.level] - 1) / map_factor;
+			a.y = randval(0, row_num[a.level] - 1) / map_factor;
+			//a.x = 31.35;
+			//a.y = 50.61;
+
+		} while (map_matrix[a.level][int(a.y * map_factor)][int(a.x * map_factor)] == 0);
+
+		a.m = 80;
+		a.vx = randval(-2, 2);
+		a.vy = randval(-2, 2);
+		a.v0 = MAX_V;
+		//a.gx = 32;
+		//a.gy = 22.5;
+		a.gx = goal[rand].x;
+		a.gy = goal[rand].y;
+		a.fgx = goal[rand].x;
+		a.fgy = goal[rand].y;
+		a.goal_level = goal[rand].level;
+		a.color = rand;
+		//cout << a.gx << " " << a.gy << endl;
+		//a.next_gx = 32;
+		//a.next_gy = 22.5;
+		a.next_gx = goal[rand].x;
+		a.next_gy = goal[rand].y;
+		a.dis = sqrt((a.x - a.gx) * (a.x - a.gx) + (a.y - a.gy) * (a.y - a.gy)) * (abs(a.goal_level-a.level)+1);
+		agent_list.push_back(a);
+	}
+
+}
+
+
+void init()
+{
+	string mapFiles[] = { "./map/data1/0.txt","./map/data1/1.txt" ,"./map/data1/2.txt" };
+	string obFiles[] = { "./map/data3/0.txt" ,"./map/data3/1.txt" ,"./map/data3/2.txt" };
+	init_obline(obFiles,total_level);
+	init_map(mapFiles,total_level);
+	for (int k = 0; k < total_level; ++k)
+	{
+		cout <<"level: "<<k<< " row_num: " << row_num[k] << " col_bnum: " << col_num[k] << endl;
+	}
+	
+	init_agent(agent_num);
+	//init_agent_seq("./map/seq.txt");
+	cout << "Init finish" << endl;
+}
+
+
+void output(AGENT* a)
+{
+	fprintf(f, "%d %g %g %d %d %d\n", a->id, a->x, a->y,a->level , a->np, a->color);
+	fprintf(ff, "%d,%g,%g,%d,%g,%g\n", a->id, a->x, a->y,a->level, a->vx, a->vy);
+}
+
+
+void step()
+{
+	total_time += tick;
+	push_new_agent();
+	fprintf(f, "%lld\n", agent_list.size());
+	fprintf(ff, "%lld\n", agent_list.size());
+
+	//单个测试用
+	//fprintf(f, "%lu\n", 2);
+
+	for (int i = 0; i < agent_list.size(); ++i)
+	{
+		AGENT* a = &agent_list[i];
+		if (a->np == 1)
+		{
+			output(a);
+			continue;
+		}
+		//first compute the desired direction;
+		double goal_dis = sqrt((a->x - a->next_gx) * (a->x - a->next_gx) + (a->y - a->next_gy) * (a->y - a->next_gy));
+		if (goal_dis == 0)goal_dis = 1e-10;
+		a->tao_1 = 0.1 + (goal_dis / a->dis) * 0.5;
+		double dx = a->v0 * (a->next_gx - a->x) / goal_dis;//期望方向向量的x
+		double dy = a->v0 * (a->next_gy - a->y) / goal_dis;
+
+		double ax = (dx - a->vx) / a->tao_1;//加速度
+		double ay = (dy - a->vy) / a->tao_1;
+
+		double total_force_x = 0;
+		double total_force_y = 0;
+
+		if (a->vx <= 0.1 && a->vy <= 0.1 && a->np != 1)
+		{
+			a->tao_1 *= ((jam_time_threshole - a->jam_time) / jam_time_threshole);
+			a->jam_time += 1;
+
+		}
+		else
+		{
+			a->jam_time = 0;
+		}
+
+
+		int agent_counter = 0;
+
+		for (int j = 0; j < agent_list.size(); ++j)
+		{
+			AGENT* b = &agent_list[j];
+			if (b->id == a->id || a->level != b->level)continue;
+			double dis = agent_dis(a, b);
+			if (dis <= sense_range)//超出感知范围不计算
+			{
+				agent_counter++;
+				agent_force(a, b, &total_force_x, &total_force_y, dis);
+			}
+		}
+		//cout << agent_counter << endl;
+		//12 25
+		if (a->jam_time >= jam_time_threshole && agent_counter > 10 && agent_counter <= 20) // 还没被挤入人群的绕路走,已经在人群里的就别搜了老老实实挤吧
+		{
+			a->jam_time = 0;
+			a->path.clear();
+			A_star(a);
+
+		}
+		else if (a->jam_time >= jam_time_threshole + 100)
+		{
+			a->jam_time = 0;
+			a->path.clear();
+			A_star(a);
+		}
+
+
+		//int id = omp_get_thread_num();
+		for (int j = 0; j < obstical_line_num[a->level]; j++)
+		{
+			obline_force(a, &obline_list[a->level][j], &total_force_x, &total_force_y);
+		}
+
+		ax += total_force_x / a->m;//加速度加上社会力的影响
+		ay += total_force_y / a->m;
+
+		a->vx += ax * tick;
+		a->vy += ay * tick;
+
+		double new_v = sqrt(a->vx * a->vx + a->vy * a->vy);//更新后的速度大小
+		if (new_v > a->v0)//超过最大速度后调整
+		{
+			a->vx = a->vx * a->v0 / new_v;
+			a->vy = a->vy * a->v0 / new_v;
+		}
+		new_v = sqrt(a->vx * a->vx + a->vy * a->vy);
+
+		a->x += a->vx * tick;
+		a->y += a->vy * tick;
+
+		if (a->x < 0)
+		{
+			a->x = 1e-10;
+		}
+		if (a->y < 0)
+		{
+			a->y = 1e-10;
+		}
+
+		if (a->path.size() > 1 && sqrt((a->x * map_factor - a->path.front().x) * (a->x * map_factor - a->path.front().x) + (a->y * map_factor - a->path.front().y) * (a->y * map_factor - a->path.front().y)) < 2 * a_step)
+		{
+
+			a->path.pop_front();
+			a->next_gx = a->path.front().x / map_factor;
+			a->next_gy = a->path.front().y / map_factor;
+		}
+		else if (a->path.size() == 1 && sqrt((a->x * map_factor - a->path.front().x) * (a->x * map_factor - a->path.front().x) + (a->y * map_factor - a->path.front().y) * (a->y * map_factor - a->path.front().y)) < 2 * a_step)
+		{
+			// 判断是否到达最终目标,是则标注已到达,否则执行上下楼函数
+			if (a->goal_level == a->level)
+			{
+				a->arrived = true;
+			}// 注意: 这里假设的是agent已经走到这一层的目标点了,默认情况下如果不是目标层,这一层的目标点一定是楼梯位
+			else if (a->level < a->goal_level)
+			{
+				a->level += 1; // 还在楼下就上楼
+				a->x = s1.down_x;
+				a->y = s1.down_y;
+				update_g(a);
+			}
+			else if (a->level > a->goal_level)
+			{
+				a->level -= 1; // 还在楼上就下楼
+				a->x = s1.up_x;
+				a->y = s1.up_y;
+				update_g(a);
+			}
+
+		}
+
+		output(a);
+
+	}
+}
+
+
+void update_density()
+{
+	for (int k = 0; k < total_level; ++k)
+	{
+		for (int i = 0; i < row_num[k]; ++i)
+		{
+
+			for (int j = 0; j < col_num[k]; ++j)
+			{
+				density_map[k][i][j] = 0;
+			}
+		}
+
+	}
+	
+	for (int i = 0; i < agent_list.size(); ++i)
+	{
+		if (agent_list[i].vx <= 0.1 && agent_list[i].vy <= 0.1)
+		{
+
+			for (int j = -3; j <= 3; ++j)
+			{
+				for (int k = -3; k <= 3; ++k)
+				{
+
+					if (in_map(int(agent_list[i].x * map_factor + j), int(agent_list[i].y * map_factor + k), agent_list[i].level))
+					{
+						density_map[agent_list[i].level][int(agent_list[i].y * map_factor + k)][int(agent_list[i].x * map_factor + j)] += 2 * (abs(j) + abs(k));
+					}
+				}
+			}
+		}
+
+
+	}
+}
+
+
+void test()
+{
+	//init_obline("./map/obstacles3.txt");
+	//init_map("./map/matrix3.txt");
+	cout << "row_num: " << row_num << " col_bnum: " << col_num << endl;
+
+	AGENT a;
+	a.id = 1;
+	a.m = 80;
+	a.gx = 1;
+	a.gy = 0;
+	a.x = 0;
+	a.y = 0;
+
+	A_star(&a);
+
+
+
+}
+
+
+void push_new_agent()
+{
+	for (int i = 0; i < agent_back_list.size(); ++i)
+	{
+		if (total_time >= agent_back_list[i].arrive_time)
+		{
+			update_g(&agent_back_list[i]);
+			agent_list.push_back(agent_back_list[i]);
+			agent_back_list.erase(agent_back_list.begin() + i);
+
+		}
+	}
+}
+
+
+
+// main
+int main()
+{
+
+
+	total_start = clock();
+
+	// omp_set_num_threads(thread_num);
+
+	init();
+
+
+	// 初始状态输出
+	fprintf(f, "%lld\n", agent_list.size());
+	fprintf(ff, "%d,%g\n", step_num, map_factor);
+	fprintf(ff, "%lld\n", agent_list.size());
+	for (int i = 0; i < agent_list.size(); ++i)
+	{
+		output(&agent_list[i]);
+	}
+
+	// 初始A★
+	int counter = 0;
+	cout << "Initializing path..." << endl;
+	for (auto& a : agent_list)
+	{
+		update_g(&a);
+		counter++;
+		printf("A_star: %d / %d \r", counter, agent_num);
+	}
+	cout << endl;
+	// 开始模拟
+	clock_t start, end;
+	cout << "Start steps..." << endl;
+	start = clock();
+	for (int i = 0; i < step_num; ++i)
+	{
+		step();
+
+		if ((i + 1) % 50 == 0)
+		{
+			update_density();
+		}
+		if ((i + 1) % 10 == 0)
+		{
+
+			end = clock();
+			printf("Step: %d / %d ;Remaining time: %d s             \r", i + 1, step_num, (end - start) * (step_num - i - 1) / 10 / 1000);
+			start = clock();
+		}
+
+	}
+	cout << endl;
+
+	total_end = clock();
+	cout << "Done " << step_num << " step(s). Total time :" << (total_end - total_start) / 1000 / 60 << " min(s)" << endl;
+
+}
---
+++ srp_3d.h
@@ -0,0 +1,586 @@
+#include<math.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include<vector>
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+// 数量限制
+#define MAX_AGENT_NUM 1000 // 最大agent数量，因为使用的是vector所以实际上没用到
+#define MAX_OBLINE_NUM 1000 // 最大障碍物数量
+
+// 建模参数
+#define MAX_V 1.02 // agent 最大速度
+#define sense_range 5 // 感知范围：m
+#define density 320 // 质量/半径，m/density = r 320
+#define tao 0.5 // 目前方向转移到目标方向的加速度参数
+
+#define total_level 3
+
+
+// sfm参数
+#define k1 120000 // body force parameter
+#define k2 240000 // tangential force parameter 切向力参数s
+#define A 2000 // N// A * exp[dis/B] only social force
+#define B 0.08 // m
+
+//行列数
+int col_num[total_level] = {};
+int row_num[total_level] = {};//init in init_map
+
+// 点阵图放大比
+double  map_factor = 10;
+
+// 程序数据
+vector<vector<vector<int>>> map_matrix; // 点阵图
+vector<vector<vector<int>>> density_map; // 人群密度图,给A*考虑选取人少的路径
+
+
+
+// 结构体
+struct cordinate {
+	double x;
+	double y;
+	int level;
+	cordinate() = default;
+	cordinate(double a, double b,int l) 
+	{
+		x = a;
+		y = b;
+		level = l;
+	}
+}; // 坐标结构体
+struct AGENT
+{
+	int id;
+
+	double m; // mass
+	double x; // pos
+	double y;
+	int level = 0;
+	double vx; // v
+	double vy;
+	double gx; // level goal pos
+	double gy;
+	double fgx; // final goal pos
+	double fgy;
+	int goal_level = 0;
+	double next_gx; // next goal pos
+	double next_gy;
+	double v0; // max velocity
+	double tao_1 = 0.1;
+
+	double dis; // 起点到终点距离
+	int jam_time = 0; // 连续多次速度过小时增加，达到设定次数时判定为卡住，重新寻路
+	int np = 0; // no path flag
+	int color = 0;
+	double arrive_time = 0;
+
+	bool arrived = 0;
+	
+	
+
+
+	list<cordinate> path; // A* 路径存储
+}; // agent 结构体
+struct OBLINE
+{
+	double sx; // start point
+	double sy;
+	double ex; // end point
+	double ey;
+	double len; // length
+
+}; // 障碍物结构体
+
+
+
+// 函数声明
+double randval(double, double);
+double agent_dis(AGENT, AGENT); // agents 之间距离
+void agent_force(AGENT*, AGENT*, double*, double*, double); // agents 之间作用力
+double point_to_line_dis(double, double, double, double, double, double, double, double*, double*); // 计算点到线距离
+void obline_force(AGENT*, OBLINE*, double*, double*); // 计算障碍物作用力
+
+
+
+// 函数实现
+double randval(double a, double b)
+{
+	return a + (b - a) * rand() / (double)RAND_MAX;
+}
+
+
+double agent_dis(AGENT* a1, AGENT* a2)
+{
+	return sqrt((a1->x - a2->x) * (a1->x - a2->x) + (a1->y - a2->y) * (a1->y - a2->y));
+}
+
+
+void agent_force(AGENT* a1, AGENT* a2, double* fx, double* fy, double dis) // a1<-a2
+{
+	// 感知距离判断放在函数调用之前了
+	if (dis == 0)
+	{
+		dis = 1e-10;//防止出现dis=0
+	}
+	double dx = (a1->x - a2->x) / dis; // a2对a1施加的力的单位方向向量的x
+	double dy = (a1->y - a2->y) / dis; // a2对a1施加的力的单位方向向量的y
+
+	//对冲判断
+	double cos1 = (a2->vx * dx + a2->vy * dy) / sqrt(dx * dx + dy * dy) / sqrt(a2->vx * a2->vx + a2->vy * a2->vy);  // a2速度和a2对a1作用力方向夹角cos
+	double sin = (a2->vx * dy - a2->vy * dx) / sqrt(dx * dx + dy * dy) / sqrt(a2->vx * a2->vx + a2->vy * a2->vy); // a2速度和a2对a1作用力方向夹角sin
+	double cos2 = (-a1->vx * dx - a1->vy * dy) / sqrt(dx * dx + dy * dy) / sqrt(a1->vx * a1->vx + a1->vy * a1->vy); // a1速度的反方向和a2对a1作用力方向夹角
+
+	if (cos1 >= 0.866 && cos2 >= 0.866)//60`
+	{
+		if (sin >= 0)
+		{
+			dx = dx + dy;
+			dy = dy - dx;
+
+		}
+		else
+		{
+			dx = dx - dy;
+			dy = dy + dx;
+
+		}
+	}
+
+	double rij = a1->m / density + a2->m / density; // 两个agent半径和
+	double delta_d = rij - dis; // 半径和减去中心点距离
+	double rif = A * exp(delta_d / B); // repulsive interaction force 斥力
+	double bf = delta_d < 0 ? 0 : k1 * delta_d; // body force agents接触时存在的力
+	// 斥力和body force的合力
+	double res_f_x = (rif + bf) * dx;
+	double res_f_y = (rif + bf) * dy;
+
+	// 切向力 agents接触时存在
+	double tfx = 0;
+	double tfy = 0;
+	if (delta_d > 0) // 身体半径有接触时
+	{
+		double tix = -dy;
+		double tiy = dx;
+		double delta_v = (a2->vx - a1->vx) * tix + (a2->vy - a1->vy) * tiy; // 切向速度差
+		tfx = k2 * delta_d * delta_v * tix;
+		tfy = k2 * delta_d * delta_v * tiy;
+	}
+	*fx += res_f_x + tfx;
+	*fy += res_f_y + tfy;
+
+	return;
+
+}
+
+
+double point_to_line_dis(double ax, double ay, double sx, double sy, double ex, double ey, double d, double* px, double* py)//p为垂足
+{
+	// 该函数需要计算垂足，用于障碍物对agent作用力方向的计算
+	double dis = 0;
+	double dot_pro = ((ax - sx) * (ex - sx) + (ay - sy) * (ey - sy)) / (d * d); // dot product of s_a and s_e divide by d2(s->start point of line;e->end point of line;a->agent)
+
+	if (dot_pro <= 0) // 垂足在start point外侧
+	{
+		dis = sqrt((ax - sx) * (ax - sx) + (ay - sy) * (ay - sy));
+		*px = sx;
+		*py = sy;
+
+	}
+	else if (dot_pro < 1) // 垂足在线段内
+	{
+		*px = sx + (ex - sx) * dot_pro;
+		*py = sy + (ey - sy) * dot_pro;
+		dis = sqrt((ax - *px) * (ax - *px) + (ay - *py) * (ay - *py));
+
+	}
+	else if (dot_pro >= 1) // 垂足在end point外侧
+	{
+		dis = sqrt((ax - ex) * (ax - ex) + (ay - ey) * (ay - ey));
+		*px = ex;
+		*py = ey;
+
+	}
+	return dis;
+
+}
+
+
+void obline_force(AGENT* a, OBLINE* l, double* fx, double* fy)
+{
+	double px, py;
+	double dis = point_to_line_dis(a->x, a->y, l->sx, l->sy, l->ex, l->ey, l->len, &px, &py);
+	if (dis <= 0)
+	{
+		dis = 1e-10;//防止出现dis=0
+	}
+	if (dis > sense_range) // 感知距离判断在函数内
+	{
+		return;
+	}
+	double riw = a->m / density - dis; // 半径-距离
+	double rif = A * exp(riw / B);
+	double bf = riw < 0 ? 0 : k1 * riw;
+	//if (bf != 0)cout << endl;
+	// 点与线作用力点的方向向量
+	double nx = (a->x - px) / dis;
+	double ny = (a->y - py) / dis;
+	// 斥力和body force合力
+	double res_f_x = (rif + bf) * nx;
+	double res_f_y = (rif + bf) * ny;
+	// 切向力
+	double tfx = 0;
+	double tfy = 0;
+	if (riw > 0)
+	{
+		double tix = -ny;
+		double tiy = nx;
+		double delta_v = a->vx * tix + a->vy * tiy;//切向速度差
+		tfx = k2 * riw * delta_v * tix;
+		tfy = k2 * riw * delta_v * tiy;
+	}
+
+	*fx += res_f_x - tfx;
+	*fy += res_f_y - tfy;
+
+
+	return;
+
+}
+
+
+//---------------------------------------------以上为SFM部分------------------------------------------------
+
+//---------------------------------------------以下为A★部分------------------------------------------------
+
+//函数声明
+void A_star(AGENT*);
+bool in_map(int, int, int);
+
+// 结构体
+struct node
+{
+	int x;
+	int y;
+	int g; // 移动代价
+	int h; // 估算成本
+	bool flag; // 这里用flag来表示node被推入close_list
+	bool check;
+
+
+
+	node* parent; // 父节点,从终点依次指向起点
+
+	node() = default;
+
+	node(int a, int b, double h = 0) {
+		flag = 0;
+		check = 0;
+		x = a;
+		y = b;
+		h = h;
+	}
+
+	/*static bool compare(node& a, node& b)
+	{
+		return (a.g + a.h) > (b.g + b.h);
+	}*/
+
+};
+
+struct dir
+{
+	int x;
+	int y;
+	dir(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+};
+
+// 数据
+vector<vector<vector<node>>> map_matrix_A;
+
+// 可调参数
+const int a_step = 5;
+vector<dir> direction = { {a_step,0}, {-a_step,0}, {0,a_step}, {0,-a_step} }; // 正向
+vector<dir> ob_direction = { {a_step,a_step},{a_step,-a_step},{-a_step,-a_step},{-a_step,a_step} }; // 斜向
+const int path_len = 2;
+const int max_time = 800; // ms
+
+
+//函数实现
+void A_star(AGENT* a)
+{
+	list<node*> open_list;
+
+	//初始化close_list 和 check
+	for (auto& y : map_matrix_A[a->level])
+	{
+		for (auto& x : y)
+		{
+			x.flag = 0;
+			x.check = 0;
+		}
+	}
+
+	// 初始化起点 目前社会力模型正确 应该是不会出现被挤出地图的情况 所以这部分可以注掉
+	/*if (!in_map(int(a->x * map_factor), int(a->y * map_factor)))
+	{
+		a->path.clear();
+		a->gx = a->x;
+		a->gy = a->y;
+		a->next_gx = a->x;
+		a->next_gy = a->y;
+		cout << "***********************************************no path" << endl;
+		a->np = 1;
+		return;
+	}*/
+	open_list.push_back(&map_matrix_A[a->level][int(a->y * map_factor)][int(a->x * map_factor)]);
+	map_matrix_A[a->level][int(a->y * map_factor)][int(a->x * map_factor)].x = int(a->x * map_factor);
+	map_matrix_A[a->level][int(a->y * map_factor)][int(a->x * map_factor)].y = int(a->y * map_factor);
+	map_matrix_A[a->level][int(a->y * map_factor)][int(a->x * map_factor)].flag = 1;
+	map_matrix_A[a->level][int(a->y * map_factor)][int(a->x * map_factor)].h = (abs(int(a->gx * map_factor) - int(a->x * map_factor)) + abs(int(a->gy * map_factor) - int(a->y * map_factor))) * 10;
+	map_matrix_A[a->level][int(a->y * map_factor)][int(a->x * map_factor)].g = 0;
+	map_matrix_A[a->level][int(a->y * map_factor)][int(a->x * map_factor)].parent = nullptr;
+
+	clock_t start, end; // 加个时间统计,用于判断无路径的情况
+	start = clock();
+
+	node* temp = open_list.front(); // 目前搜索的点
+
+
+	while (!(abs(temp->x - a->gx * map_factor) <= a_step && abs(temp->y - a->gy * map_factor) <= a_step))
+	{
+		double f = INT_MAX;
+		//找F最小的节点
+		for (auto* a : open_list)
+		{
+			if ((a->g + a->h) < f)
+			{
+				f = (a->g + a->h);
+				temp = a;
+			}
+		}
+
+		bool flag = 0;
+		// 判断周围是否有墙
+		for (auto d : direction)
+		{
+			if (in_map(temp->x + d.x, temp->y + d.y,a->level) && map_matrix[a->level][temp->y + d.y][temp->x + d.x] == 0)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		for (auto d : ob_direction)
+		{
+			if (in_map(temp->x + d.x, temp->y + d.y,a->level) && map_matrix[a->level][temp->y + d.y][temp->x + d.x] == 0)
+			{
+				flag = 1;
+				break;
+			}
+		}
+
+		// 正方向上搜索,此时移动代价为10
+		for (auto d : direction)
+		{
+			if (in_map(temp->x + d.x, temp->y + d.y,a->level))
+			{
+				int point_type = map_matrix[a->level][temp->y + d.y][temp->x + d.x];
+				//if (density_map[temp->y + d.y][temp->x + d.x])point_type == 0;
+				//cout << density_map[temp->y + d.y][temp->x + d.x] << endl;
+				if (point_type != 0 && map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].check == 0)//节点在地图内，且不在障碍物部分,且未check过
+				{
+					if (map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].flag == 0)//表示该点第一次进入open_list
+					{
+						map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].x = temp->x + d.x;
+						map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].y = temp->y + d.y;
+						map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].h = (abs(int(a->gx * map_factor) - (temp->x + d.x)) + abs(int(a->gy * map_factor) - (temp->y + d.y))) * 10;
+						map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].g = temp->g + 10 * a_step * (point_type * 0.75 + density_map[a->level][temp->y + d.y][temp->x + d.x]);
+						map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].parent = temp;
+						map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].flag = 1;
+					}
+					else // 再次筛查一个点d,只有在通过(这次搜索的点temp到达d的代价)比(d的父节点到d)更小时,才会更新
+					{
+						if (temp->g + 10 * a_step < map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].g)
+						{
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].g = temp->g + 10 * a_step * (point_type * 0.75 + density_map[a->level][temp->y + d.y][temp->x + d.x]);
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].parent = temp;
+						}
+					}
+					open_list.push_back(&map_matrix_A[a->level][temp->y + d.y][temp->x + d.x]);
+				}
+			}
+
+		}
+
+		// 斜方向上搜索,此时移动代价为14,实际上应该是10*2^(1/2),小于这个值会导致结果更偏向斜向移动
+		if (flag == 0)
+		{
+			for (auto d : ob_direction)
+			{
+				if (in_map(temp->x + d.x, temp->y + d.y,a->level))
+				{
+					int point_type = map_matrix[a->level][temp->y + d.y][temp->x + d.x];
+					//if (density_map[temp->y + d.y][temp->x + d.x])point_type == 0;
+					if (point_type != 0 && map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].check == 0)//节点在地图内，且不在障碍物部分,且未check过 && map_matrix_A[temp->y + d.y][temp->x + d.x].check == 0
+					{
+						if (map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].flag == 0) // 表示该点第一次进入open_list
+						{
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].x = temp->x + d.x;
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].y = temp->y + d.y;
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].h = (abs(int(a->gx * map_factor) - (temp->x + d.x)) + abs(int(a->gy * map_factor) - (temp->y + d.y))) * 10;
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].g = temp->g + 14 * a_step * (point_type * 0.75 + density_map[a->level][temp->y + d.y][temp->x + d.x]);
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].parent = temp;
+							map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].flag = 1;
+						}
+						else // 再次筛查一个点d,只有在通过(这次搜索的点temp到达d的代价)比(d的父节点到d)更小时,才会更新
+						{
+							if (temp->g + 14 * a_step < map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].g)
+							{
+								map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].g = temp->g + 14 * a_step * (point_type * 0.75 + density_map[a->level][temp->y + d.y][temp->x + d.x]);
+								map_matrix_A[a->level][temp->y + d.y][temp->x + d.x].parent = temp;
+							}
+						}
+						open_list.push_back(&map_matrix_A[a->level][temp->y + d.y][temp->x + d.x]);
+					}
+				}
+			}
+		}
+
+		open_list.remove(temp);
+		temp->check = 1; // 已经检查过这个点
+
+		// 无路径判断,绝大部分在n s之内可以搜到,超过视为不存在路径
+		end = clock();
+		if (end - start > max_time)
+		{
+			a->path.clear();
+			a->gx = a->x;
+			a->gy = a->y;
+			a->next_gx = a->x;
+			a->next_gy = a->y;
+			a->np = 1;
+			return;
+		}
+
+	}
+	// push 路径到 path,这里可以选择路径密度
+	int counter = 0;
+	do
+	{
+		counter++;
+		if (counter == path_len) // 每n个取一个点
+		{
+			a->path.push_front(cordinate(temp->x, temp->y,a->level));
+			counter = 0;
+		}
+		else if (map_matrix[a->level][temp->y][temp->x] == 2) // 门口重要节点也要push
+		{
+			a->path.push_front(cordinate(temp->x, temp->y,a->level));
+		}
+		if (temp->parent == nullptr)break;
+		temp = temp->parent;
+
+	} while (temp != nullptr);
+	if (!a->path.empty())
+	{
+		a->next_gx = a->path.front().x / map_factor;
+		a->next_gy = a->path.front().y / map_factor;
+	}
+	else
+	{
+		a->next_gx = a->gx;
+		a->next_gy = a->gy;
+	}
+
+}
+
+
+bool in_map(int x, int y,int level)
+{
+	return (x >= 0 && x < col_num[level] - 1) && (y >= 0 && y < row_num[level] - 1);
+}
+
+
+//---------------------------------------------以上为A★部分------------------------------------------------
+
+//---------------------------------------------以下为3D部分-------------------------------------------------
+
+// 结构体
+struct stair
+{
+	int id;
+	double up_x;
+	double up_y;
+	double down_x;
+	double down_y;
+	stair(int i,double ux, double uy, double dx, double dy)
+	{
+		id = i;
+		up_x = ux;
+		up_y = uy;
+		down_x = dx;
+		down_y = dy;
+	}
+};
+stair s1(0,85,7.6,85,11);
+
+
+// 函数声明
+void update_g(AGENT*);
+
+
+// 函数实现
+
+// 到达楼层目标后判断
+void update_g(AGENT* a)
+{	// 目前的情况
+	
+	if (a->level == a->goal_level)
+	{   // 已经到同层了就搜索
+		a->gx = a->fgx;
+		a->gy = a->fgy;
+		a->path.clear();
+		A_star(a);
+	}
+	else if(a->level < a->goal_level)
+	{
+		// 更新gx到这一层的上楼点,目前只写楼梯,只写一个楼梯
+		//cout << "here" << endl;
+		a->gx = s1.up_x;
+		a->gy = s1.up_y;
+		a->path.clear();
+		A_star(a);
+		
+	}
+	else if (a->level > a->goal_level)
+	{
+		// 更新gx到这一层的下楼点
+		a->gx = s1.down_x;
+		a->gy = s1.down_y;
+		a->path.clear();
+		A_star(a);
+		
+	}
+
+}
+
+// 走楼梯
+void use_stair(AGENT* a)
+{
+
+
+}
+
+// 走电梯
+void use_lift(AGENT* a)
+{
+
+}
